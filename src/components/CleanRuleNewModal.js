2b45ed063010b093997fe4359441fa786d675e53
米亚 <futuretask@126.com>
2024-06-03 17:04:00 +0800

    add func

--- src/components/CleanRuleNewModal.js
+++ src/components/CleanRuleNewModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button, Checkbox, Flex, Input, Layout, Modal, Radio, Select, Space, Steps, Table, Tabs, Col, Row, message, theme } from 'antd';
 import { FormOutlined, SearchOutlined } from '@ant-design/icons';
 
@@ -23,6 +23,69 @@ const CleanRuleNewModal = (props, onClose) => {
         props.onClose();
         setOpen(false);
     };
+
+    const initialItems = [
+        {
+          label: 'Tab 1',
+          children: 'Content of Tab 1',
+          key: '1',
+        },
+        {
+          label: 'Tab 2',
+          children: 'Content of Tab 2',
+          key: '2',
+        },
+        {
+          label: 'Tab 3',
+          children: 'Content of Tab 3',
+          key: '3',
+          closable: false,
+        },
+      ];
+
+      const [activeKey, setActiveKey] = useState(initialItems[0].key);
+  const [items, setItems] = useState(initialItems);
+  const newTabIndex = useRef(0);
+  const onChange = (newActiveKey) => {
+    setActiveKey(newActiveKey);
+  };
+  const add = () => {
+    const newActiveKey = `newTab${newTabIndex.current++}`;
+    const newPanes = [...items];
+    newPanes.push({
+      label: 'New Tab',
+      children: 'Content of new Tab',
+      key: newActiveKey,
+    });
+    setItems(newPanes);
+    setActiveKey(newActiveKey);
+  };
+  const remove = (targetKey) => {
+    let newActiveKey = activeKey;
+    let lastIndex = -1;
+    items.forEach((item, i) => {
+      if (item.key === targetKey) {
+        lastIndex = i - 1;
+      }
+    });
+    const newPanes = items.filter((item) => item.key !== targetKey);
+    if (newPanes.length && newActiveKey === targetKey) {
+      if (lastIndex >= 0) {
+        newActiveKey = newPanes[lastIndex].key;
+      } else {
+        newActiveKey = newPanes[0].key;
+      }
+    }
+    setItems(newPanes);
+    setActiveKey(newActiveKey);
+  };
+  const onEdit = (targetKey, action) => {
+    if (action === 'add') {
+      add();
+    } else {
+      remove(targetKey);
+    }
+  };
  
     return (
         <Modal
@@ -37,7 +100,7 @@ const CleanRuleNewModal = (props, onClose) => {
             ]}
         >
             <div class="flex-col-cont" style={{height: 450, width: '100%', border: '1px solid red'}}>
-                aaaa
+                <Tabs type="editable-card" onChange={onChange} activeKey={activeKey} onEdit={onEdit} items={items} />
             </div>
         </Modal>
     );
