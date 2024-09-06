ca0bcdec88fe0393e0e6d1325516a0c88a0aeccd
米亚 <futuretask@126.com>
2024-09-06 16:06:15 +0800

    add func

--- src/js/config.js
+++ src/js/config.js
@@ -1,8 +1,3 @@
-export const BACKEND_CONTEXT_PATH = '/teamachinebackend'
-export const DEV_BACKEND_BASE_URL = 'http://localhost:8080' + BACKEND_CONTEXT_PATH;
-export const PROD_BACKEND_BASE_URL = 'https://langtuo.cartisan.top:446' + BACKEND_CONTEXT_PATH;;
-export const BACKEND_BASE_URL = DEV_BACKEND_BASE_URL; // process.env.NODE_ENV === 'dev' ? devBaseURL: prodBaseURL;
-
 export const CONSOLE_CONTEXT_PATH = '/teamachineconsole'
 
 export const TIMEOUT = 5000;
@@ -12,3 +7,8 @@ export const OSS_CONFIG_REGION = 'oss-cn-hangzhou';
 export const OSS_CONFIG_TEA_FOLDER = 'teamachine/tea';
 export const OSS_CONFIG_SERIES_FOLDER = 'teamachine/series';
 export const OSS_CONFIG_MENU_FOLDER = 'teamachine/folder';
+
+export const BACKEND_CONTEXT_PATH = '/teamachinebackend'
+export const DEV_BACKEND_BASE_URL = 'http://localhost:8080' + BACKEND_CONTEXT_PATH;
+export const PROD_BACKEND_BASE_URL = 'https://langtuo.cartisan.top:446' + BACKEND_CONTEXT_PATH;;
+export const BACKEND_BASE_URL = DEV_BACKEND_BASE_URL; // process.env.NODE_ENV === 'dev' ? devBaseURL: prodBaseURL;
